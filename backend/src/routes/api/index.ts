f7177705ea241d9cf3bd5372f0347fe89310662f
amauri <brown817@msu.edu>
2024-12-23 18:53:57 -0500

    route update

---
+++ backend/src/routes/api/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { packsRouter } from './packsRoutes.js';
+import { userRouter } from './userRoutes.js';
+
+const router = Router();
+
+router.use('/packs', packsRouter);
+router.use('/users', userRouter);
+
+export default router;
