eb50d1de1f7ab1e69477a7bbb88f44bb1d7ee963
amauri <brown817@msu.edu>
2025-01-07 08:54:23 -0500

    added the mongo database, don't forget to add the link to the environment file when rendering

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URL || '');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://atlas-sql-67721de73744c17d3c446955-smxms.a.query.mongodb.net/CustomPacksDB?ssl=true&authSource=admin');
 
 export default mongoose.connection;
